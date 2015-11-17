b2075e18f69a964f14bf3b82b054d1cd20752eb8
MrFunfantastic <brendancinelli@hotmail.com>
2015-11-17 15:38:07 -0500

    New Stuff

---
+++ GameDevProj/Assets/Scripts/ScreamCollider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreamCollider : MonoBehaviour {
+
+    // Reference to audio sources
+    AudioSource mScream;
+    
+    bool check;
+
+	// Use this for initialization
+	void Start () {
+	    mScream = GetComponent<AudioSource>();
+        check = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (check == true)
+        {
+            check = false;
+            mScream.Play();
+            Destroy(GameObject.FindWithTag("Sarah"));
+        }
+        
+
+    }
+
+}
