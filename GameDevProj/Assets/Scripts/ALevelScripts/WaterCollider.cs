ecd0f16e1a10359fbbae5ad4230f235ba9ace9cd
Alexandre Charron <charronalexandre@hotmail.com>
2015-11-30 01:45:51 -0500

    using git shell to try and pull and push

--- GameDevProj/Assets/Scripts/ALevelScripts/WaterCollider.cs
+++ GameDevProj/Assets/Scripts/ALevelScripts/WaterCollider.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 
 public class WaterCollider : MonoBehaviour {
-
-    public Vector3 position;
-    public Vector3 lookAt;
+    
+    
     
     // Use this for initialization
 	void Start () {
@@ -20,13 +19,7 @@ public class WaterCollider : MonoBehaviour {
     {
         if (other.name == "MainCharacter")
         {
-            GameObject player = other.gameObject;
-
-            player.transform.position = position/*new Vector3(-15.56f, 11.68f, 12.34f)*/;
-
-            player.transform.rotation.SetLookRotation(lookAt/*new Vector3(0, 299.355f, 0)*/);
-
-            SanitySystem.increaseInsanity(2000);
+            other.gameObject.GetComponentInChildren<MainCharacterScript>().die();
             
         }
     }
