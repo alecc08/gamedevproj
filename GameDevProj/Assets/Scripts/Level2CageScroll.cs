a6b41db33c7b8991b085c9c6a3d0c0420a11aadd
MrFunfantastic <brendancinelli@hotmail.com>
2015-11-30 11:55:50 -0500

    Level 2

---
+++ GameDevProj/Assets/Scripts/Level2CageScroll.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class Level2CageScroll : MonoBehaviour, Readable {
+	
+	public string GetText()
+	{
+		return "(E)veryone is a\nprisoner of their\npast";
+	}
+}
---
+++ GameDevProj/Assets/Scripts/RoomScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class RoomScript : MonoBehaviour, Readable  {
+	
+	public string GetText()
+	{
+		return "(L)ook what I drew daddy";
+	}
+}
