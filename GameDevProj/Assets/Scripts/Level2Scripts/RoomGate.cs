d74f33da904bb1ecaca0d2a18a4334230ac32458
MrFunfantastic <brendancinelli@hotmail.com>
2015-11-30 13:39:06 -0500

    Minor changes to level 2

---
+++ GameDevProj/Assets/Scripts/Level2Scripts/RoomGate.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+using System;
+
+public class RoomGate : MonoBehaviour, Interactable {
+
+	[SerializeField]
+	GameObject roomLight;
+
+	AudioSource[] creepyKid;
+	GameObject roomGate;
+	GameObject cabinetDoorL, cabinetDoorR;
+	bool openCabinetDoor;
+
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0f;
+		openCabinetDoor = false;
+		creepyKid = GetComponents<AudioSource>();
+		roomGate = GameObject.FindGameObjectWithTag ("roomGate");
+		cabinetDoorL = GameObject.FindGameObjectWithTag ("cabinetDoor");
+		cabinetDoorR = GameObject.FindGameObjectWithTag ("cabinetDoor2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+
+		if (openCabinetDoor == true) {
+		
+			if(timer < 5f){
+				cabinetDoorL.transform.position += new Vector3(0.2f,0,0)* Time.deltaTime;		
+				cabinetDoorR.transform.position += new Vector3(-0.07f,0,0) * Time.deltaTime;
+			}
+			
+
+
+
+		}
+	}
+	
+	public void interact(GameObject player)
+	{
+		Destroy (roomLight);
+		roomGate.transform.position = new Vector3 (99.789f, -.022f, 85.293f);
+		openCabinetDoor = true;
+		creepyKid[0].Play ();
+		creepyKid [1].Play ();
+		timer = 0f;
+	}
+	
+	public bool isInteractable()
+	{
+		return true;
+	}
+	
+	public string getLabel()
+	{
+		return "Examine Poster";
+	}
+}
+
---
+++ GameDevProj/Assets/Scripts/Level2Scripts/RoomScript2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class RoomScript2 : MonoBehaviour, Readable  {
+	
+	public string GetText()
+	{
+		return "(P)lease don't be mad\n at me";
+	}
+}
