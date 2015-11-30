2b445f5883d16fb7d32542ef9aed03a1f396f03a
Alexandre Charron <charronalexandre@hotmail.com>
2015-11-30 03:54:29 -0500

    ending alex_scene sends player to ALevel

---
+++ GameDevProj/Assets/nextScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "MainCharacter")
+        {
+            Application.LoadLevel("ALevel");
+
+        }
+    }
+}
